d26e6fa20f62fdab0e95ba0c55e7a6957ce7a32d
gsl <wmns109@gmail.com>
2021-12-08 07:25:22 +0900

    Jetpack Compose State 1

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
@@ -21,7 +21,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import com.codelabs.state.ui.StateCodelabTheme
+import androidx.compose.runtime.getValue
 
 class TodoActivity : AppCompatActivity() {
 
@@ -33,8 +36,21 @@ class TodoActivity : AppCompatActivity() {
             StateCodelabTheme {
                 Surface {
                     // TODO: build the screen in compose
+                    TodoActivityScreen(todoViewModel)
                 }
             }
         }
     }
+
+}
+
+
+@Composable
+private fun TodoActivityScreen(todoViewModel: TodoViewModel) {
+    val items : List<TodoItem> by todoViewModel.todoItems.observeAsState(listOf()) // in the next steps we'll complete this
+    TodoScreen(
+        items = items,
+        onAddItem = {todoViewModel.addItem(it) }, // in the next steps we'll complete this
+        onRemoveItem = { todoViewModel.removeItem(it)} // in the next steps we'll complete this
+    )
 }
